9b04bc70433066380c1e2499236f49119fd75272
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-25 21:45:41 +0100

    Refactor function name and return type

--- challenge-1-ctrl-c__ctrl-v/cut-copy-paste.test.js
+++ challenge-1-ctrl-c__ctrl-v/cut-copy-paste.test.js
@@ -1,21 +1,21 @@
-const { findFirstActionAndIndex } = require('./cut-copy-paste');
+const { findFirstCommandAndIndex } = require('./cut-copy-paste');
 
-describe('findFirstActionAndIndex', () => {
+describe('findFirstCommandAndIndex', () => {
   it('should return CTRL+C and correct index', () => {
     const input = 'The first[CTRL+C]';
-    const output = { copy: 9 };
-    expect(findFirstActionAndIndex(input)).toStrictEqual(output);
+    const output = ['copy', 9];
+    expect(findFirstCommandAndIndex(input)).toStrictEqual(output);
   });
 
   it('should return CTRL+C and correct index', () => {
     const input = 'The first[CTRL+C] [CTRL+V]';
-    const output = { copy: 9 };
-    expect(findFirstActionAndIndex(input)).toStrictEqual(output);
+    const output = ['copy', 9];
+    expect(findFirstCommandAndIndex(input)).toStrictEqual(output);
   });
 
   it('should return CTRL+C and correct index', () => {
     const input = 'The first challenge[CTRL+C]';
-    const output = { copy: 19 };
-    expect(findFirstActionAndIndex(input)).toStrictEqual(output);
+    const output = ['copy', 19];
+    expect(findFirstCommandAndIndex(input)).toStrictEqual(output);
   });
 });
