e26fe7df26cc455c5de48e281563aa171c610bf1
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-25 21:34:14 +0100

    Add new tests and improve the code

--- challenge-1-ctrl-c__ctrl-v/cut-copy-paste.js
+++ challenge-1-ctrl-c__ctrl-v/cut-copy-paste.js
@@ -1,5 +1,8 @@
-function findFirstActionAndIndex() {
-  return { copy: 9 };
+function findFirstActionAndIndex(inputText) {
+  const copyIndex = inputText.indexOf('[CTRL+C]');
+  const pasteIndex = inputText.indexOf('[CTRL+V]');
+
+  return { copy: copyIndex };
 }
 
 module.exports = {
--- challenge-1-ctrl-c__ctrl-v/cut-copy-paste.test.js
+++ challenge-1-ctrl-c__ctrl-v/cut-copy-paste.test.js
@@ -6,4 +6,16 @@ describe('findFirstActionAndIndex', () => {
     const output = { copy: 9 };
     expect(findFirstActionAndIndex(input)).toStrictEqual(output);
   });
+
+  it('should return CTRL+C and correct index', () => {
+    const input = 'The first[CTRL+C] [CTRL+V]';
+    const output = { copy: 9 };
+    expect(findFirstActionAndIndex(input)).toStrictEqual(output);
+  });
+
+  it('should return CTRL+C and correct index', () => {
+    const input = 'The first[CTRL+V] [CTRL+C]';
+    const output = { copy: 18 };
+    expect(findFirstActionAndIndex(input)).toStrictEqual(output);
+  });
 });
