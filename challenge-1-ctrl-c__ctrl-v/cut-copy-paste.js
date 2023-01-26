73bbec050d96e5f7ac8cea2903a70471d99fa745
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-26 08:51:24 +0100

    Add  copy/paste index logic

--- challenge-1-ctrl-c__ctrl-v/cut-copy-paste.js
+++ challenge-1-ctrl-c__ctrl-v/cut-copy-paste.js
@@ -1,10 +1,25 @@
-function findFirstActionAndIndex(inputText) {
-  const copyIndex = inputText.indexOf('[CTRL+C]');
-  const pasteIndex = inputText.indexOf('[CTRL+V]');
+function findFirstCommandAndIndex(inputText) {
+  const copyIndex =
+    inputText.indexOf('[CTRL+C]') > -1
+      ? inputText.indexOf('[CTRL+C]')
+      : Infinity;
+  const pasteIndex =
+    inputText.indexOf('[CTRL+V]') > -1
+      ? inputText.indexOf('[CTRL+V]')
+      : Infinity;
 
-  return { copy: copyIndex };
+  const commandAndIndex = ['nothing', -1];
+  if (copyIndex < pasteIndex) {
+    commandAndIndex[0] = 'copy';
+    commandAndIndex[1] = copyIndex;
+  } else if (pasteIndex < copyIndex) {
+    commandAndIndex[0] = 'paste';
+    commandAndIndex[1] = pasteIndex;
+  }
+
+  return commandAndIndex;
 }
 
 module.exports = {
-  findFirstActionAndIndex,
+  findFirstCommandAndIndex,
 };
