9e884ed40f1d63162913b7a6e7d80366031cb27d
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-28 22:32:39 +0100

    Refactor

--- challenge-1-ctrl-c__ctrl-v/cut-copy-paste.js
+++ challenge-1-ctrl-c__ctrl-v/cut-copy-paste.js
@@ -1,24 +1,20 @@
-function findIndexOfCommand(inputText, command) {
-  return inputText.indexOf(command) > -1
-    ? inputText.indexOf(command)
-    : Infinity;
-}
+const findIndexOfCommand = (inputText, command) =>
+  inputText.indexOf(command) > -1 ? inputText.indexOf(command) : Infinity;
 
 function findFirstCommandAndIndex(inputText) {
   const copyIndex = findIndexOfCommand(inputText, '[CTRL+C]');
   const pasteIndex = findIndexOfCommand(inputText, '[CTRL+V]');
   const cutIndex = findIndexOfCommand(inputText, '[CTRL+X]');
 
+  //find smallest command index
   const firstCommand = [
     { command: 'copy', index: copyIndex },
     { command: 'paste', index: pasteIndex },
     { command: 'cut', index: cutIndex },
-  ]
-    .filter((command) => command.index !== Infinity)
-    .sort((a, b) => a.index - b.index)[0];
-  console.log({ firstCommand });
-  const commandAndIndex = ['nothing', -1];
-  if (firstCommand?.command) {
+  ].sort((a, b) => a.index - b.index)[0];
+
+  const commandAndIndex = ['nothing', -1]; //default
+  if (firstCommand.index !== Infinity) {
     commandAndIndex[0] = firstCommand.command;
     commandAndIndex[1] = firstCommand.index;
   }
@@ -52,7 +48,7 @@ function challenge(input) {
   return input;
 }
 
-// challenge('the first[CTRL+X] [CTRL+V]');
+challenge('the first[CTRL+X] [CTRL+V]');
 module.exports = {
   findIndexOfCommand,
   findFirstCommandAndIndex,
