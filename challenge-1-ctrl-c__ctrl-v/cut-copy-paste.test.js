f2c971745c41348b7b04d2500bfb443f97e09fdc
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-29 11:31:48 +0100

    Add test for data set

--- challenge-1-ctrl-c__ctrl-v/cut-copy-paste.test.js
+++ challenge-1-ctrl-c__ctrl-v/cut-copy-paste.test.js
@@ -139,3 +139,47 @@ describe('challenge : ', () => {
     expect(challenge(input)).toEqual(output);
   });
 });
+
+// TEST DATA
+const dataSet = [
+  {
+    input: 'the big red[CTRL+C] fox jumps over [CTRL+V] lazy dog.',
+    expected: 'the big red fox jumps over the big red lazy dog.',
+  },
+  {
+    input: '[CTRL+V]the tall oak tree towers over the lush green meadow.',
+    expected: 'the tall oak tree towers over the lush green meadow.',
+  },
+  {
+    input: 'the sun shines down[CTRL+C] on [CTRL+V][CTRL+C] the busy [CTRL+V].',
+    expected:
+      'the sun shines down on the sun shines down the busy the sun shines down on the sun shines down.',
+  },
+  {
+    input: '[CTRL+V]the tall oak tree towers over the lush green meadow.',
+    expected: 'the tall oak tree towers over the lush green meadow.',
+  },
+  {
+    input: 'a majestic lion[CTRL+C] searches for [CTRL+V] in the tall grass.',
+    expected: 'a majestic lion searches for a majestic lion in the tall grass.',
+  },
+  {
+    input:
+      'the shimmering star[CTRL+X]Twinkling in the dark, [CTRL+V] shines bright.',
+    expected: 'Twinkling in the dark, the shimmering star shines bright.',
+  },
+  {
+    input:
+      '[CTRL+X]a fluffy white cloud drifts [CTRL+V][CTRL+C] across the sky, [CTRL+V]',
+    expected:
+      'a fluffy white cloud drifts  across the sky, a fluffy white cloud drifts ',
+  },
+];
+describe.each(dataSet)(
+  'SHOULD ANALYZE THE DATA SET: ',
+  ({ input, expected }) => {
+    it(`INPUT: ${input} \n should return ${expected}`, () => {
+      expect(challenge(input)).toEqual(expected);
+    });
+  }
+);
