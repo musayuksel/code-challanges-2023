1e2fc1068ecc1c35d0f5914f015ebb1e8824d0c8
Musa <79163385+musayuksel@users.noreply.github.com>
2023-02-17 13:19:30 +0000

    Add basic style

--- challenge-2-langtons-ant/src/App.jsx
+++ challenge-2-langtons-ant/src/App.jsx
@@ -3,12 +3,26 @@ import './App.css';
 import Cell from './components/Cell';
 
 function App() {
+  const boardStyles = (boardWidth) => ({
+    display: 'grid',
+    gridTemplateColumns: `repeat(${boardWidth}, 1fr)`,
+    gridGap: '1px',
+    border: '1px solid black',
+    width: '500px',
+    height: '500px',
+  });
   const [board, setBoard] = useState([
-    [false, false],
-    [false, false],
+    [false, false, false, false],
+    [false, false, false, false],
+    [false, true, false, false],
+    [false, false, false, false],
   ]);
 
-  return <div className='App'>{board.map((row) => row.map(Cell))}</div>;
+  return (
+    <div style={boardStyles(4)} className='App'>
+      {board.map((row) => row.map((cell) => <Cell cell={cell} />))}
+    </div>
+  );
 }
 
 export default App;
--- challenge-2-langtons-ant/src/components/Cell.jsx
+++ challenge-2-langtons-ant/src/components/Cell.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
-export default function Cell() {
+export default function Cell({ cell }) {
+  console.log({ cell });
   const cellStyle = {
+    backgroundColor: `${cell ? 'black' : 'white'}`,
     border: '1px solid black',
+    color: 'white',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   };
-  return <div style={cellStyle}>Cell</div>;
+  return <div style={cellStyle}></div>;
 }
