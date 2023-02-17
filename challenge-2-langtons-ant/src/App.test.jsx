07b49037e3fd3a3c2c677442afba59c9845a5159
Musa <79163385+musayuksel@users.noreply.github.com>
2023-02-17 13:48:38 +0000

    Add test for oneUp util function

--- challenge-2-langtons-ant/src/App.test.jsx
+++ challenge-2-langtons-ant/src/App.test.jsx
@@ -1,10 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
-import {
-  boardWith,
-  countMinesAroundCell,
-  minesweeper,
-} from './utils/minesweeper';
+import { oneUp } from './utils/antMoves';
 
 const board = [
   [false, false, false, false],
@@ -17,34 +13,13 @@ const ant = {
   direction: 'N',
 };
 
-describe('Mine Sweeper', () => {
-  it('should calculate the board width correctly', () => {
-    const width = boardWith(board);
-    expect(width).toBe(5);
-  });
-
-  it('should correctly count the mines around the cell', () => {
-    const topLeftCorner = countMinesAroundCell(board, 0, 5);
-    expect(topLeftCorner).toBe('0');
-
-    const secondRowLeftCorner = countMinesAroundCell(board, 5, 5);
-    expect(secondRowLeftCorner).toBe('2');
-
-    const topRightCorner = countMinesAroundCell(board, 4, 5);
-    expect(topRightCorner).toBe('0');
-
-    const forthRowRightCorner = countMinesAroundCell(board, 19, 5);
-    expect(forthRowRightCorner).toBe('1');
-  });
-
-  it('should render the board correctly', () => {
-    const boardResult = minesweeper(board);
-    expect(boardResult).toEqual(expectedBoard);
-  });
-
-  it('should render 5*5 board for home page', () => {
-    render(<App />);
-    const board = screen.getAllByTestId('board-cell');
-    expect(board).toHaveLength(25);
+describe('Utils functions :', () => {
+  it('should find the oneUp index of ant', () => {
+    const antNextPosition = oneUp(ant);
+    const antNew = {
+      currentPosition: [1, 1],
+      direction: 'N',
+    };
+    expect(antNextPosition).toEqual(antNew);
   });
 });
