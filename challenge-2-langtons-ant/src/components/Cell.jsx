00e6e47f1ba7cd61a32f19c249e7aebec7e4d582
Musa <79163385+musayuksel@users.noreply.github.com>
2023-02-20 16:46:55 +0000

    Add Ant direction for cells

--- challenge-2-langtons-ant/src/components/Cell.jsx
+++ challenge-2-langtons-ant/src/components/Cell.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
-export default function Cell({ cell }) {
-  console.log({ cell });
+export default function Cell({ cell, currentAnt }) {
+  //   console.log(currentAnt);
   const cellStyle = {
     backgroundColor: `${cell ? 'black' : 'white'}`,
     border: '1px solid black',
-    color: 'white',
+    color: `${cell ? 'white' : 'black'}`,
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
   };
-  return <div style={cellStyle}></div>;
+
+  return <div style={cellStyle}>{currentAnt.currentDirection}</div>;
 }
