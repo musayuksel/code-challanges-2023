2b298573ca7f23abd8a40920e248bc486bc6dc86
Musa <79163385+musayuksel@users.noreply.github.com>
2023-02-21 12:04:12 +0000

    Add ant image to the component

--- challenge-2-langtons-ant/src/components/Cell.jsx
+++ challenge-2-langtons-ant/src/components/Cell.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
-import { cellStyle } from '../styles/Cell.style';
-
+import { antImageStyle, cellStyle } from '../styles/Cell.style';
+import antImage from '../assets/ant.svg';
 export default function Cell({ cell, currentAnt }) {
-  return <div style={cellStyle(cell)}>{currentAnt.currentDirection}</div>;
+  return (
+    <div style={cellStyle(cell)}>
+      {currentAnt && (
+        <img
+          style={antImageStyle(currentAnt.currentDirection)}
+          src={antImage}
+          alt='A red cartoon ant'
+        />
+      )}
+    </div>
+  );
 }
