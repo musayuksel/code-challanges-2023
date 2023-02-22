91ed31333e94ff9bdce4b84a02bf724bc823ac1d
Musa <79163385+musayuksel@users.noreply.github.com>
2023-02-22 21:47:57 +0000

    Refactor : Move board to different component

--- challenge-2-langtons-ant/src/App.jsx
+++ challenge-2-langtons-ant/src/App.jsx
@@ -1,64 +1,7 @@
-import { useState, useEffect } from 'react';
-import Cell from './components/Cell';
-import { findNextMovesOfAnt } from './utils/findNextMovesOfAnt';
-import { boardStyles } from './styles/App.style';
-import { invertColourOfLeavingCell } from './utils/invertColourOfLeavingCell';
-import { initialAnt, initialBoard } from './utils/initialBoardAndAnt';
-import { expandGrid } from './utils/expandGrid';
+import Board from './components/Board';
 
 function App() {
-  const [board, setBoard] = useState(initialBoard);
-  const [currentAnt, setCurrentAnt] = useState(initialAnt);
-
-  useEffect(() => {
-    function moveOneStep() {
-      const [currentAntRow, currentAntCol] = currentAnt.currentPosition;
-      const nextMovesOfAnt = findNextMovesOfAnt(
-        currentAnt,
-        board[currentAntRow][currentAntCol]
-      );
-      setCurrentAnt(nextMovesOfAnt);
-      setBoard((prevBoard) =>
-        invertColourOfLeavingCell(prevBoard, currentAntRow, currentAntCol)
-      );
-    }
-    const callMoveStepsEveryOneSec = (time) => {
-      setTimeout(() => {
-        moveOneStep();
-        if (
-          currentAnt.currentPosition[0] <= 0 ||
-          currentAnt.currentPosition[0] >= board.length - 1 ||
-          currentAnt.currentPosition[1] <= 0 ||
-          currentAnt.currentPosition[1] >= board.length - 1
-        ) {
-          expandGrid(setCurrentAnt, setBoard);
-        }
-      }, time);
-    };
-    callMoveStepsEveryOneSec(2);
-  }, [board, currentAnt]);
-
-  const drawBoard = board.map((row, rowIndex) =>
-    row.map((cell, cellIndex) => (
-      <Cell
-        key={`${rowIndex}-${cellIndex}`}
-        currentAnt={
-          currentAnt.currentPosition.join('-') === `${rowIndex}-${cellIndex}`
-            ? currentAnt
-            : undefined
-        }
-        cell={cell}
-      />
-    ))
-  );
-
-  return (
-    <>
-      <div style={boardStyles(board.length)} className='App'>
-        {drawBoard}
-      </div>
-    </>
-  );
+  return <Board speed={1000} />;
 }
 
 export default App;
---
+++ challenge-2-langtons-ant/src/components/Board.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react';
+import { boardStyles } from '../styles/App.style';
+import { expandGrid } from '../utils/expandGrid';
+import { findNextMovesOfAnt } from '../utils/findNextMovesOfAnt';
+import { initialAnt, initialBoard } from '../utils/initialBoardAndAnt';
+import { invertColourOfLeavingCell } from '../utils/invertColourOfLeavingCell';
+import Cell from './Cell';
+export default function Board({ speed }) {
+  const [board, setBoard] = useState(initialBoard);
+  const [currentAnt, setCurrentAnt] = useState(initialAnt);
+
+  useEffect(() => {
+    function moveOneStep() {
+      const [currentAntRow, currentAntCol] = currentAnt.currentPosition;
+      const nextMovesOfAnt = findNextMovesOfAnt(
+        currentAnt,
+        board[currentAntRow][currentAntCol]
+      );
+      setCurrentAnt(nextMovesOfAnt);
+      setBoard((prevBoard) =>
+        invertColourOfLeavingCell(prevBoard, currentAntRow, currentAntCol)
+      );
+    }
+    const callMoveStepsEveryOneSec = (time) => {
+      setTimeout(() => {
+        moveOneStep();
+        if (
+          currentAnt.currentPosition[0] <= 0 ||
+          currentAnt.currentPosition[0] >= board.length - 1 ||
+          currentAnt.currentPosition[1] <= 0 ||
+          currentAnt.currentPosition[1] >= board.length - 1
+        ) {
+          expandGrid(setCurrentAnt, setBoard);
+        }
+      }, time);
+    };
+    callMoveStepsEveryOneSec(speed);
+  }, [board, currentAnt, speed]);
+
+  const drawBoard = board.map((row, rowIndex) =>
+    row.map((cell, cellIndex) => (
+      <Cell
+        key={`${rowIndex}-${cellIndex}`}
+        currentAnt={
+          currentAnt.currentPosition.join('-') === `${rowIndex}-${cellIndex}` &&
+          currentAnt
+        }
+        cell={cell}
+      />
+    ))
+  );
+
+  return (
+    <>
+      <div style={boardStyles(board.length)} className='App'>
+        {drawBoard}
+      </div>
+    </>
+  );
+}
