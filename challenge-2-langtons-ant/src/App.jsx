6a6f8bd2c4e1f7d458ad4d15ac0a88d5b41f101b
Musa <79163385+musayuksel@users.noreply.github.com>
2023-02-23 08:32:11 +0000

    Add speed selector button

--- challenge-2-langtons-ant/src/App.jsx
+++ challenge-2-langtons-ant/src/App.jsx
@@ -1,7 +1,42 @@
+import { useState } from 'react';
 import Board from './components/Board';
 
 function App() {
-  return <Board speed={1000} />;
+  const [speed, setSpeed] = useState(1000);
+  const [isStart, setIsStart] = useState(false);
+
+  function handleSpeedChange(event) {
+    setSpeed(+event.target.value);
+  }
+  function handleStart() {
+    setIsStart(true);
+  }
+
+  return (
+    <main>
+      {isStart ? (
+        <Board speed={speed} />
+      ) : (
+        <section className='select-box'>
+          <select
+            onChange={handleSpeedChange}
+            id='select-menu'
+            className='select-menu'
+          >
+            <option value={1000}> Choose speed</option>
+            <option value={1}>Very Fast</option>
+            <option value={200}>Fast</option>
+            <option value={500}>Medium</option>
+            <option value={700}>Slow</option>
+            <option value={1200}>Very Slow</option>
+          </select>
+          <button className='start-btn' onClick={handleStart}>
+            Start
+          </button>
+        </section>
+      )}
+    </main>
+  );
 }
 
 export default App;
