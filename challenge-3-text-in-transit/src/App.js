f57752fe1e9187714286effa38b59a5b61491ca5
Musa <79163385+musayuksel@users.noreply.github.com>
2023-03-16 15:19:42 +0000

    Add screen to app

--- challenge-3-text-in-transit/src/App.js
+++ challenge-3-text-in-transit/src/App.js
@@ -1,16 +1,33 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import "./App.css";
 import TextInput from "./components/TextInput";
+import Screen from "./components/Screen";
+import { scroll } from "./utils/scroll";
 
 function App() {
-  const [currentText, setCurrentText] = useState("AND Digital");
+  const [currentScrollingText, setCurrentScrollingText] =
+    useState("AND Digital");
+  const [screenWidth, setScreenWidth] = useState(14);
+  // const textForScreen = scroll(currentScrollingText, 10);
 
+  const textCharsForScreen = useMemo(
+    () => scroll(currentScrollingText, screenWidth),
+    [currentScrollingText, screenWidth]
+  );
   const handleScroolClick = () => {};
   return (
     <div className="App">
       <h1>Text in Transit</h1>
-      <TextInput currentText={currentText} setCurrentText={setCurrentText} />
+      <TextInput
+        currentScrollingText={currentScrollingText}
+        setCurrentScrollingText={setCurrentScrollingText}
+      />
       <button onClick={handleScroolClick}>Scroll</button>
+      <Screen
+        currentScrollingText={currentScrollingText}
+        textCharsForScreen={textCharsForScreen}
+        screenWidth={screenWidth}
+      />
     </div>
   );
 }
