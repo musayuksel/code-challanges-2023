a5ceef3aa9238ac1f12c1a8cacb252ea56f489ae
Musa <79163385+musayuksel@users.noreply.github.com>
2023-03-17 15:07:31 +0000

    Add style for page

--- challenge-3-text-in-transit/src/components/TextInput.jsx
+++ challenge-3-text-in-transit/src/components/TextInput.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import "./TextInput.style.css";
 export default function TextInput({
   setCurrentScrollingText,
   setScreenWidth,
@@ -17,33 +17,42 @@ export default function TextInput({
   };
   return (
     <form>
-      <label htmlFor="text">Enter Your Text:</label>
-      <input
-        id="text"
-        type="text"
-        placeholder="Enter your text here"
-        value={currentTextInput}
-        onChange={(e) => setCurrentTextInput(e.target.value)}
-      />
-      <label htmlFor="screenWidth">Enter Screen Width:</label>
-      <input
-        id="screenWidth"
-        type="number"
-        min={1}
-        value={currentScreenWidth}
-        onChange={(e) => {
-          setCurrentScreenWidth(+e.target.value);
-        }}
-      />
-      <input
-        id="speed"
-        type="number"
-        min={1}
-        value={currentSpeed}
-        onChange={(e) => {
-          setCurrentSpeed(+e.target.value);
-        }}
-      />
+      <div className="form-section text-input">
+        <label htmlFor="text">Enter Your Text:</label>
+        <textarea
+          id="text"
+          type="text"
+          rows={5}
+          cols={30}
+          placeholder="Enter your text here"
+          value={currentTextInput}
+          onChange={(e) => setCurrentTextInput(e.target.value)}
+        />
+      </div>
+      <div className="form-section">
+        <label htmlFor="screenWidth">Enter Screen Width:</label>
+        <input
+          id="screenWidth"
+          type="number"
+          min={1}
+          value={currentScreenWidth}
+          onChange={(e) => {
+            setCurrentScreenWidth(+e.target.value);
+          }}
+        />
+      </div>
+      <div className="form-section">
+        <label htmlFor="speed">Enter Text Speed:</label>
+        <input
+          id="speed"
+          type="number"
+          min={1}
+          value={currentSpeed}
+          onChange={(e) => {
+            setCurrentSpeed(+e.target.value);
+          }}
+        />
+      </div>
       <button onClick={handleScrollClick}>Scroll</button>
     </form>
   );
