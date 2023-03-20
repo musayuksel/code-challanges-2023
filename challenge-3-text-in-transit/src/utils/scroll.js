a715188435f6532997542817fa26de67bc794a33
Musa <79163385+musayuksel@users.noreply.github.com>
2023-03-20 23:19:11 +0000

    Delete comments

--- challenge-3-text-in-transit/src/utils/scroll.js
+++ challenge-3-text-in-transit/src/utils/scroll.js
@@ -6,20 +6,7 @@ export const scroll = (text, screenWidth = 4) => {
 
   const extraSpaces = " ".repeat(screenWidth);
   text = extraSpaces + text + extraSpaces;
-  const textsForScroll = [
-    // {
-    //   letter: "x",
-    //   bold: false,
-    //   underline: false,
-    //   color: "red",
-    // },
-    // {
-    //   letter: "y",
-    //   bold: false,
-    //   underline: false,
-    //   color: "red",
-    // },
-  ];
+  const textsForScroll = [];
   let index = 0;
 
   const styles = {
@@ -31,13 +18,7 @@ export const scroll = (text, screenWidth = 4) => {
     colorEndIndexes: [],
   };
   // "Welcome on board this service to [B]London[/B]. Please have [U]all[/U] tickets and passes ready for inspection. This service is expected to depart [C:#00FF00]on time[/C]"
-  //find all B indexes and add them to an array
-  //find all U indexes and add them to an array
-  //find all C indexes and add them to an array
-  //find all /B indexes and add them to an array
-  //find all /U indexes and add them to an array
-  //find all /C indexes and add them to an array
-  //and replace them with empty spaces
+
   while (text.includes("[B]")) {
     const index = text.indexOf("[B]");
     styles.boldStartIndexes.push(index);
@@ -55,7 +36,6 @@ export const scroll = (text, screenWidth = 4) => {
     text = text.replace("[/U]", "");
   }
 
-  console.log({ styles });
   text.split("").forEach((letter, index) => {
     const newLetter = {
       letter: letter,
@@ -86,21 +66,3 @@ export const scroll = (text, screenWidth = 4) => {
 
   return result;
 };
-// export const scroll = (text, screenWidth = 4) => {
-//   //add empty space to the left and right of the text amount of screenWidth
-//   //start from the left side and get screenWidth number of characters
-//   //then move one character to the right and get screenWidth number of characters
-//   //do it until the end of the text
-
-//   const extraSpaces = " ".repeat(screenWidth);
-//   text = extraSpaces + text + extraSpaces;
-//   const textsForScroll = [];
-//   let index = 0;
-
-//   while (index < text.length - screenWidth + 1) {
-//     textsForScroll.push(text.slice(index, index + screenWidth));
-//     index++;
-//   }
-//   console.log({ textsForScroll });
-//   return textsForScroll;
-// };
