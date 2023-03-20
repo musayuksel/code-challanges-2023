da4b413f4a6e88da5552020c53757b303232a126
Musa <79163385+musayuksel@users.noreply.github.com>
2023-03-20 23:18:37 +0000

    Refactor

--- challenge-3-text-in-transit/src/components/Screen.jsx
+++ challenge-3-text-in-transit/src/components/Screen.jsx
@@ -8,52 +8,39 @@ export default function Screen({
 }) {
   const [currentTextIndex, setCurrentTextIndex] = useState(0);
   useEffect(() => {
-    // update the text on the screen every 500ms
+    // update the text on the screen every 1000ms as a default
     const interval = setTimeout(() => {
       setCurrentTextIndex((prevIndex) =>
         prevIndex === textCharsForScreen.length - 1 ? 0 : prevIndex + 1
       );
-    }, 10000 / speed);
+    }, 1000 / speed);
     return () => clearInterval(interval);
   }, [textCharsForScreen, currentTextIndex, speed]);
 
-  //   const screenBoxes = useMemo(
-  //     () =>
-  //       Array.from({ length: screenWidth }, (_, i) => (
-  //         //   textCharsForScreen[currentTextIndex]?.split("").map((char, i) => (
-  //         <div
-  //           key={i}
-  //           className="screen-box"
-  //           style={{ textDecoration: `underline` }}
-  //         >
-  //           {textCharsForScreen[currentTextIndex]?.[i]}
-  //           {/* {char} */}
-  //         </div>
-  //       )),
-  //     [textCharsForScreen, currentTextIndex, screenWidth]
-  //   );
   const currentText = textCharsForScreen[currentTextIndex];
-  const screenBoxes = currentText?.map((letter, index) => (
-    <div
-      key={index}
-      className="screen-box"
-      style={{
-        textDecoration: `${letter.underline ? "underline" : ""}`,
-        fontWeight: `${letter.bold ? "bold" : ""}`,
-      }}
-    >
-      {letter.letter}
-    </div>
-  ));
+  const screenBoxes = useMemo(
+    () =>
+      currentText?.map((letter, index) => (
+        <div
+          key={index}
+          className="screen-box"
+          style={{
+            textDecoration: `${letter.underline ? "underline" : ""}`,
+            fontWeight: `${letter.bold ? "bold" : ""}`,
+            color: `${letter.color}`,
+          }}
+        >
+          {letter.letter}
+        </div>
+      )),
+    [currentText]
+  );
 
-  console.log({ screenBoxes });
   return (
     <section
       className="scroll-screen"
       style={{ gridTemplateColumns: `repeat(${screenWidth},1fr)` }}
     >
-      {/* {textCharsForScreen[currentTextIndex]}
-       */}
       {screenBoxes}
     </section>
   );
