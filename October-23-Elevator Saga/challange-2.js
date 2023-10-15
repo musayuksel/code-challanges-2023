abb36364ba4078e0701b1f2bfa120e7db06577d8
Musa <79163385+musayuksel@users.noreply.github.com>
2023-10-15 19:16:09 +0100

    Refactor

--- October-23-Elevator Saga/challange-2.js
+++ October-23-Elevator Saga/challange-2.js
@@ -9,48 +9,34 @@ const challenge2 = {
           ...new Set(
             elevator.destinationQueue.filter((flNum) => flNum !== floorNum)
           ),
-        ];
-        //  elevator.destinationQueue = elevator.getPressedFloors()
+        ]; //delete duplicates
         elevator.checkDestinationQueue();
-
-        elevator.goToFloor(floorNum, true);
+        elevator.goToFloor(floorNum, true); //stop immediately
       }
     });
 
     // when a floor button is pressed, go to that floor
-    elevator.on('floor_button_pressed', (floorNum) => {
-      elevator.goToFloor(floorNum);
-      console.log(
-        `PAssenger pressed for Floor:${floorNum} elevator.destinationQueue: `,
-        elevator.destinationQueue
-      );
-    });
+    elevator.on('floor_button_pressed', (floorNum) =>
+      elevator.goToFloor(floorNum)
+    );
 
     // when someone is waiting on a floor to go up, go to that floor
-    floors.forEach((floor) => {
+    floors.forEach((floor) =>
       floor.on('up_button_pressed', () => {
         elevator.goToFloor(floor.floorNum());
         elevator.destinationQueue = [...new Set(elevator.destinationQueue)];
         elevator.checkDestinationQueue();
-        console.log(
-          `Up button pressed from Floor:${floor.floorNum()} elevator.destinationQueue: `,
-          elevator.destinationQueue
-        );
-      });
-    });
+      })
+    );
 
     // when someone is waiting on a floor to go down, go to that floor
-    floors.forEach((floor) => {
+    floors.forEach((floor) =>
       floor.on('down_button_pressed', () => {
         elevator.goToFloor(floor.floorNum());
         elevator.destinationQueue = [...new Set(elevator.destinationQueue)];
         elevator.checkDestinationQueue();
-        console.log(
-          `Down button pressed from Floor:${floor.floorNum()} elevator.destinationQueue: `,
-          elevator.destinationQueue
-        );
-      });
-    });
+      })
+    );
   },
 
   update: function (dt, elevators, floors) {
