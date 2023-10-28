a87b15b84c9ea2520a932218170860ab3ba0f416
Musa <79163385+musayuksel@users.noreply.github.com>
2023-10-28 13:11:16 +0100

    refactor

--- October-23-Elevator Saga/challange-3.js
+++ October-23-Elevator Saga/challange-3.js
@@ -4,32 +4,28 @@ const challenge3 = {
     let elevatorDirection = 'down';
     let goingUpQueue = [];
     let goingDownQueue = [];
+    const MAX_FLOOR = floors.length - 1;
 
     const elevator = elevators[0];
 
-    elevator.on('idle', function () {
-      console.log('elevator is IDLE>>>>>>>>>>>>>>>>>>>>>>>>');
+    elevator.on('floor_button_pressed', (floorNum) =>
+      floorNum > elevator.currentFloor()
+        ? addFloorToGoingUpQueue(floorNum)
+        : addFloorToGoingDownQueue(floorNum)
+    );
+
+    floors.forEach((floor) => {
+      floor.on('down_button_pressed', () =>
+        addFloorToGoingDownQueue(floor.floorNum())
+      );
+
+      floor.on('up_button_pressed', () =>
+        addFloorToGoingUpQueue(floor.floorNum())
+      );
     });
 
-    function runElevator() {
-      console.log({ elevatorDirection });
-      elevator.goingUpIndicator(elevatorDirection === 'up');
-      elevator.goingDownIndicator(elevatorDirection === 'down');
-
-      if (elevatorDirection === 'up') {
-        elevator.destinationQueue = [4]; //goingUpQueue;
-        //goingUpQueue=[]//TODO change with the remain floors
-        elevator.checkDestinationQueue();
-      } else if (elevatorDirection === 'down') {
-        elevator.destinationQueue = [0]; // goingDownQueue;
-        //goingDownQueue=[]//TODO change with the remain floors
-        elevator.checkDestinationQueue();
-      }
-    }
-
-    elevator.on('stopped_at_floor', function (floorNum) {
-      console.log('STOPPED AT FLOOR', floorNum);
-      if (floorNum === 4 || floorNum === 0) {
+    elevator.on('stopped_at_floor', (floorNum) => {
+      if (floorNum === MAX_FLOOR || floorNum === 0) {
         elevatorDirection = elevatorDirection === 'up' ? 'down' : 'up';
         runElevator();
       }
@@ -37,35 +33,37 @@ const challenge3 = {
 
     elevator.on('passing_floor', function (floorNum, direction) {
       if (direction === 'up' && goingUpQueue.includes(floorNum)) {
-        console.log('Elevator is going UP and some is on floor ', floorNum);
         elevator.goToFloor(floorNum, true);
-        goingUpQueue = goingUpQueue.filter((f) => f !== floorNum);
+        // goingUpQueue = goingUpQueue.filter((f) => f !== floorNum);
+        goingUpQueue = deleteFloorFromQueue(floorNum, goingUpQueue);
       }
       if (direction === 'down' && goingDownQueue.includes(floorNum)) {
-        console.log('Elevator is going DOWN and some is on floor ', floorNum);
         elevator.goToFloor(floorNum, true);
-        goingDownQueue = goingDownQueue.filter((f) => f !== floorNum);
-      }
-      // console.log({goingUpQueue,goingDownQueue})
-    });
-
-    elevator.on('floor_button_pressed', function (floorNum) {
-      if (floorNum > elevator.currentFloor()) {
-        addFloorToGoingUpQueue(floorNum);
-      }
-      if (floorNum < elevator.currentFloor()) {
-        addFloorToGoingDownQueue(floorNum);
+        // goingDownQueue = goingDownQueue.filter((f) => f !== floorNum);
+        goingDownQueue = deleteFloorFromQueue(floorNum, goingDownQueue);
       }
     });
 
-    floors.forEach((floor) => {
-      floor.on('down_button_pressed', function () {
-        addFloorToGoingDownQueue(floor.floorNum());
-      });
+    function deleteFloorFromQueue(floorNum, queue) {
+      return queue.filter((f) => f !== floorNum);
+    }
 
-      floor.on('up_button_pressed', function () {
-        addFloorToGoingUpQueue(floor.floorNum());
-      });
+    function runElevator() {
+      console.log({ elevatorDirection });
+      elevator.goingUpIndicator(elevatorDirection === 'up');
+      elevator.goingDownIndicator(elevatorDirection === 'down');
+
+      if (elevatorDirection === 'up') {
+        elevator.destinationQueue = [MAX_FLOOR];
+        elevator.checkDestinationQueue();
+      } else if (elevatorDirection === 'down') {
+        elevator.destinationQueue = [0];
+        elevator.checkDestinationQueue();
+      }
+    }
+
+    elevator.on('idle', function () {
+      console.log('elevator is IDLE>>>>>>>>>>>>>>>>>>>>>>>>');
     });
 
     function addFloorToGoingUpQueue(floorNumber) {
